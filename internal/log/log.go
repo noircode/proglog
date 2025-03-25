3b05f272ed8cec0b51ffef794378bab262313006
Kang Noir <simaul.noir@gmail.com>
2025-03-26 01:23:27 +0700

    add functionality to log stuct

--- internal/log/log.go
+++ internal/log/log.go
@@ -1,14 +1,24 @@
 package log
 
-import "sync"
+import (
+	"fmt"
+	"os"
+	"path"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+
+	api "github.com/noircode/proglog/api/v1"
+)
 
 type Log struct {
 	mu sync.RWMutex
 
-	Dir string
-	Config Config
+	Dir           string
+	Config        Config
 	activeSegment *segment
-	segments []*segment
+	segments      []*segment
 }
 
 func NewLog(dir string, c Config) (*Log, error) {
@@ -19,10 +29,75 @@ func NewLog(dir string, c Config) (*Log, error) {
 		c.Segment.MaxIndexBytes = 1024
 	}
 	l := &Log{
-		Dir: dir,
+		Dir:    dir,
 		Config: c,
 	}
-	
+
 	return l, l.setup()
 }
 
+func (l *Log) setup() error {
+	files, err := os.ReadDir(l.Dir)
+	if err != nil {
+		return err
+	}
+	var baseOffset []uint64
+	for _, file := range files {
+		offStr := strings.TrimSuffix(
+			file.Name(),
+			path.Ext(file.Name()),
+		)
+		off, _ := strconv.ParseUint(offStr, 10, 0)
+		baseOffset = append(baseOffset, off)
+	}
+	sort.Slice(baseOffset, func(i, j int) bool {
+		return baseOffset[i] < baseOffset[j]
+	})
+	for i := 0; i < len(baseOffset); i++ {
+		if err = l.newSegment(baseOffset[i]); err != nil {
+			return err
+		}
+		// baseOffset contains dup for index and store so we skip
+		// the dup
+		i++
+	}
+	if l.segments == nil {
+		if err = l.newSegment(
+			l.Config.Segment.InitialOffset,
+		); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (l *Log) Append(record *api.Record) (uint64, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	off, err := l.activeSegment.Append(record)
+	if err != nil {
+		return 0, err
+	}
+	if l.activeSegment.IsMaxed() {
+		err = l.newSegment(off + 1)
+	}
+	return off, err
+}
+
+func (l *Log) Read(off uint64) (*api.Record, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	var s *segment
+	for _, segment := range l.segments {
+		if segment.baseOffset <= off && off < segment.nextOffset {
+			s = segment
+			break
+		}
+	}
+	if s == nil || s.nextOffset <= off {
+		return nil, fmt.Errorf("offset out of range: %d", off)
+	}
+	return s.Read(off)
+}
